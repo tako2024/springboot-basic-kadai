cc133d332d80925a2a399c78a38042726f60e4f3
tako2024 <iyoda551@gmail.com>
2025-04-25 22:49:26 +0900

    更新

--- src/main/java/com/example/springkadaitodo/service/ToDoService.java
+++ src/main/java/com/example/springkadaitodo/service/ToDoService.java
@@ -1,10 +1,11 @@
-package com.example.springkadaitodo.repository;
+package com.example.springkadaitodo.service;
 
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.example.springkadaitodo.entity.ToDo;
+import com.example.springkadaitodo.repository.ToDoRepository;
 
 @Service
 public class ToDoService {

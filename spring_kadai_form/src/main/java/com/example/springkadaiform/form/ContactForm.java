252e772c52cd00ad93925e73cc50dfcaca63a88d
tako2024 <iyoda551@gmail.com>
2025-05-06 15:06:53 +0900

    Add files via upload

--- spring_kadai_form/src/main/java/com/example/springkadaiform/form/ContactForm.java
+++ spring_kadai_form/src/main/java/com/example/springkadaiform/form/ContactForm.java
@@ -12,7 +12,7 @@ public class ContactForm {
 	private String name;
 
 	//メールアドレス
-	@NotBlank(message = "メールアドレスの入力形式が正しくありません。")
+	@NotBlank(message = "メールアドレスを入力してください。")
 	@Email(message = "メールアドレスの入力形式が正しくありません。")
 	private String email;
 
